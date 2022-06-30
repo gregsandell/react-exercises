87e0b2b6a547605f3c4875d82de49fa40a304bff
Greg Sandell <gsandell_99@yahoo.com>
2022-06-30 13:20:53 -0700

    Add routing + nav links for the three tasks; remove unused logos

--- src/containers/App/index.js
+++ src/containers/App/index.js
@@ -1,3 +1,4 @@
+import { BrowserRouter as Router, Routes, Route, NavLink } from 'react-router-dom'
 import './App.css'
 import ToDoList from '../../components/toDoList'
 import Counter from '../../components/counter'
@@ -6,11 +7,27 @@ import suggestions from './autocompleteData'
 
 function App () {
   return (
-    <div className='App'>
-      <ToDoList />
-      <Counter />
-      <Autocomplete suggestions={suggestions} />
-    </div>
+    <Router>
+      <div className='App'>
+        <ul>
+          <li>
+            <NavLink to='/todolist' exact='true' activestyle={{ color: 'green' }}>To Do List</NavLink>
+          </li>
+          <li>
+            <NavLink to='/autocomplete' exact='true' activestyle={{ color: 'green' }}>Autocomplete</NavLink>
+          </li>
+          <li>
+            <NavLink to='/counter' exact='true' activestyle={{ color: 'green' }}>Counter</NavLink>
+          </li>
+        </ul>
+        <Routes>
+          <Route exact='true' path='/' element={<ToDoList />} />
+          <Route exact='true' path='/todolist' element={<ToDoList />} />
+          <Route exact='true' path='/autocomplete' element={<Autocomplete suggestions={suggestions} />} />
+          <Route exact='true' path='/counter' element={<Counter />} />
+        </Routes>
+      </div>
+    </Router>
   )
 }
 
