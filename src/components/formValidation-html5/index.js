54571cb50190c7ba5e00a4bc029eb8073b4a6ba4
Greg Sandell <gsandell_99@yahoo.com>
2022-08-08 12:58:14 -0700

    Simplify the email pattern, use React ref and .reportValidity()

--- src/components/formValidation-html5/index.js
+++ src/components/formValidation-html5/index.js
@@ -9,16 +9,20 @@
  *
  */
 
-import { useState } from 'react'
+import { useState, createRef } from 'react'
 import './formValidationHtml5.css'
 
 export default function FormValidationHtml5 () {
+  const formRef = createRef()
   const [state, setState] = useState({
     email: '',
     firstName: '',
     lastName: ''
   })
 
+  const validate = () => {
+    return formRef.current.reportValidity()
+  }
   function handleOnEmailChange (event) {
     const email = event.target.value
     setState({
@@ -28,9 +32,10 @@ export default function FormValidationHtml5 () {
   }
 
   /* eslint-disable no-control-regex */
-  // const emailPattern = '(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])'
+  const emailPattern = '^.+@.+\\..+$'
   const namePattern = '^([^0-9]*)$'
   // const namePattern = '^.*$'
+
   function handleOnFirstnameChange (event) {
     const firstName = event.target.value
     setState({
@@ -48,25 +53,26 @@ export default function FormValidationHtml5 () {
 
   const handleSubmit = (e) => {
     e.preventDefault()
+    validate()
     console.log('submit')
   }
 
   return (
     <div className='main'>
-      <form onSubmit={handleSubmit}>
+      <form onSubmit={handleSubmit} ref={formRef}>
         <h1>Please enter your name and email</h1>
         <div className='field'>
           <label htmlFor='email'>Email</label>
           {/* Note: the HTML5 type=email is pretty poor, it accepts "foo@bar" as a valid email! */}
-          <input id='email' required value={state.email} type='email' onChange={handleOnEmailChange} />
+          <input id='email' name='email' placeholder='Email (e.g. name@company.com)' required pattern={emailPattern} value={state.email} type='email' onChange={handleOnEmailChange} />
         </div>
         <div className='field'>
           <label>First name</label>
-          <input value={state.firstName} required pattern={namePattern} type='text' onChange={handleOnFirstnameChange} />
+          <input value={state.firstName} name='firstName' placeholder='First name (no numbers)' required pattern={namePattern} type='text' onChange={handleOnFirstnameChange} />
         </div>
         <div className='field'>
           <label>Last name</label>
-          <input value={state.lastName} required pattern={namePattern} onChange={handleOnLastnameChange} />
+          <input value={state.lastName} name='lastName' placeholder='Last name (no numbers)' required pattern={namePattern} onChange={handleOnLastnameChange} />
         </div>
         <button type='submit'>Submit</button>
         <div>
