c1f0cede0b335eb10e82ff9f60df184ee6050cf1
Greg Sandell <gsandell_99@yahoo.com>
2022-08-08 22:12:20 -0700

    Add finishing touches to <FormValidationHtml5>

--- src/components/formValidation-html5/index.js
+++ src/components/formValidation-html5/index.js
@@ -2,26 +2,33 @@ import { useState, createRef } from 'react'
 import './formValidationHtml5.css'
 
 export default function FormValidationHtml5 () {
-  const formRef = createRef()
-  const emailRef = createRef()
   const [state, setState] = useState({
     email: '',
     firstName: '',
     lastName: ''
   })
+  const [submitted, setSubmitted] = useState(false)
+  const formRef = createRef()
+  const emailRef = createRef()
   const formValidationMessages = {
     email: 'Please enter a complete email address, such as person@company.com',
     firstName: 'First Name may not contain any numbers',
     lastName: 'Last Name may not contain any numbers'
   }
+  /* Note: don't use the native HTML5 type=email pattern validation, it accepts "foo@bar" as a valid email! */
+  const emailPattern = '^.+@.+\\..+$'
+  const namePattern = '^([^0-9]*)$'
 
   const validate = () => {
+    let allValid = true
     for (let i = 0; i < formRef.current.length; i++) { // Note: formRef.current.map() doesn't work
       const elem = formRef.current[i]
       if (!elem.validity.valid && elem.validity.patternMismatch && elem.name in formValidationMessages) {
         elem.setCustomValidity(formValidationMessages[elem.name])
+        allValid = false
       }
     }
+    setSubmitted(allValid)
 
     return formRef.current.reportValidity()
   }
@@ -31,14 +38,11 @@ export default function FormValidationHtml5 () {
       ...state,
       email
     })
+    setSubmitted(false)
     if (!event.target.validity.patternMismatch) {
       emailRef.current.setCustomValidity('') // string must be empty or it registers as invalid
     }
   }
-
-  /* Note: don't use the native HTML5 type=email pattern validation, it accepts "foo@bar" as a valid email! */
-  const emailPattern = '^.+@.+\\..+$'
-  const namePattern = '^([^0-9]*)$'
 
   function handleOnFirstnameChange (event) {
     const firstName = event.target.value
@@ -46,6 +50,7 @@ export default function FormValidationHtml5 () {
       ...state,
       firstName
     })
+    setSubmitted(false)
     if (!event.target.validity.patternMismatch) {
       event.target.setCustomValidity('')
     }
@@ -56,6 +61,7 @@ export default function FormValidationHtml5 () {
       ...state,
       lastName
     })
+    setSubmitted(false)
     if (!event.target.validity.patternMismatch) {
       event.target.setCustomValidity('')
     }
@@ -63,8 +69,17 @@ export default function FormValidationHtml5 () {
 
   const handleSubmit = (e) => {
     console.log('submit')
+    setSubmitted(true)
     validate()
     e.preventDefault()
+  }
+  const handleCancel = () => {
+    setState({
+      email: '',
+      firstName: '',
+      lastName: ''
+    })
+    setSubmitted(false)
   }
 
   return (
@@ -84,10 +99,12 @@ export default function FormValidationHtml5 () {
           <input value={state.lastName} name='lastName' id='firstName' placeholder='Last name (no numbers)' required pattern={namePattern} onChange={handleOnLastnameChange} />
         </div>
         <input type='submit' onClick={handleSubmit} />
-        <div>
+        <button id='cancel' onClick={handleCancel}>Cancel</button>
+        {submitted && (<div>
           <div>Submitted Name: {state.firstName} {state.lastName}</div>
           <div>Submitted Email: {state.email}</div>
         </div>
+        )}
       </form>
     </div>
   )
