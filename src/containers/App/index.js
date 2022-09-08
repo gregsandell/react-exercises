3ea9ef647cdd89355588784ed6b8c0ef552ae7ac
Greg Sandell <gsandell_99@yahoo.com>
2022-09-08 09:08:48 -0700

    Add form validation demo using Formik; switch from npm to yarn;

--- src/containers/App/index.js
+++ src/containers/App/index.js
@@ -14,6 +14,7 @@ import Quiz from '../../components/quiz'
 import FormValidation from '../../components/formValidation'
 import FormValidationHtml5 from '../../components/formValidation-html5'
 import FormValidationHooks from '../../components/formValidation-hooks'
+import FormValidationFormik from '../../components/formValidationFormik'
 import ColorSwitch from '../../components/colorSwitch'
 import InfiniteScroll from '../../components/infiniteScroll'
 import Calc from '../../components/calc'
@@ -40,6 +41,7 @@ function App () {
     { to: '/formValidation', text: 'Form Validation', element: <FormValidation /> },
     { to: '/formValidation-html5', text: 'Form Validation with HTML5', element: <FormValidationHtml5 /> },
     { to: '/formValidation-hooks', text: 'Form Validation with react-form-hooks', element: <FormValidationHooks /> },
+    { to: '/formValidationFormik', text: 'Form Validation with Formik', element: <FormValidationFormik /> },
     { to: '/colorSwitch', text: 'Color Switch', element: <ColorSwitch /> },
     { to: '/calc', text: 'Calculator', element: <Calc initial={0} /> },
     { to: '/infiniteScroll', text: 'Infinite Scroll', element: <InfiniteScroll /> }
@@ -56,6 +58,7 @@ function App () {
             )
           }
         </ul>
+        {/* TO DO Make the apps centered in screen */}
         <Routes>
           {
             navlinks.map((record, i) => (
